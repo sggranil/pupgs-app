2b1836f7de13e7ca7ffdb65b05dec4752f4159bf
sggranil <dev.simongranil@gmail.com>
2025-01-30 00:42:21 +0800

    :hammer: add auth middleware

---
+++ src/services/Middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function middleware(req: Request) {
+    const cookieStore = await cookies(); 
+    const sessionToken = cookieStore.get('session');
+
+    if (!sessionToken) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    return NextResponse.next();
+}
