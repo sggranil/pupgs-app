d8b5455494883f57a3c1e528548f25a2f63f3b51
sggranil <dev.simongranil@gmail.com>
2025-02-05 06:31:51 +0800

    :construction: student landing page

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,9 +1,48 @@
 "use client";
+import { useState } from "react";
 
+import Modal from "@/components/organisms/Modal";
 import React from "react";
+import AddThesis from "@/components/organisms/AddThesis";
 
 export default function StudentDashboard() {
+    const [enrolledSubjectModal, setEnrolledSubjectModal] = useState<boolean>(false);
+    const [thesisProposalModal, setThesisProposalModal] = useState<boolean>(false);
+
     return (
-        <h1>This is the dashboard for student</h1>
+        <div>
+            <div className="w-full px-2 pt-6 pb-12">
+                <div className="flex align-center justify-between py-2 border-b border-gray-200">
+                    <h1 className="text-textPrimary text-xl font-bold p-2">
+                        Currently Enrolled Subject
+                    </h1>
+                    <button
+                        onClick={() => setEnrolledSubjectModal(true)}
+                        className="px-3 py-2 text-sm font-normal rounded-md whitespace-nowrap bg-bgPrimary text-white"
+                    >
+                        Upload Documents
+                    </button>
+                </div>
+            </div>
+            <div className="w-full px-2 pt-6 pb-12">
+                <div className="flex align-center justify-between py-2 border-b border-gray-200">
+                    <h1 className="text-textPrimary text-xl font-bold p-2">
+                        Your Thesis
+                    </h1>
+                    <button
+                        onClick={() => setThesisProposalModal(true)}
+                        className="px-3 py-2 text-sm font-normal rounded-md whitespace-nowrap bg-bgPrimary text-white"
+                    >
+                        Add a Thesis Proposal
+                    </button>
+                </div>
+            </div>
+            <Modal title="Upload Documents" isModalOpen={enrolledSubjectModal} setModalOpen={setEnrolledSubjectModal}>
+                <AddThesis />
+            </Modal>
+            <Modal title="Add Thesis" isModalOpen={thesisProposalModal} setModalOpen={setThesisProposalModal}>
+                <AddThesis />
+            </Modal>
+        </div>
     );
 }
