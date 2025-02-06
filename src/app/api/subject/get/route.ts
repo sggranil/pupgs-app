dbdf126285c6146e8b80ae6811ff3ed8b56617f5
sggranil <dev.simongranil@gmail.com>
2025-02-07 05:51:58 +0800

    create api for subject confirmation

--- src/app/api/subject/get/route.ts
+++ src/app/api/subject/get/route.ts
@@ -3,12 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest,
-    { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest) {
     try {
         const subject = await prisma.enrolledSubject.findMany({
-            where: { student_id: parseInt(params.id) },
+            include: {
+                student: {
+                    include: {
+                        user: true,
+                    }
+                },
+            }
         });
 
         if (!subject) {
