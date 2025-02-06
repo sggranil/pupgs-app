11fe9e6be8e8679a24d8570755c51944c8a01cec
sggranil <dev.simongranil@gmail.com>
2025-02-06 14:54:41 +0800

    re-config interfaces

--- src/interface/thesis.interface.ts
+++ src/interface/thesis.interface.ts
@@ -5,19 +5,33 @@ export interface Thesis {
     thesis_title: string;
     student: Student;
     student_id: number;
-    adviser: Adviser;
-    adviser_id: number;
-    is_confirmed: boolean;
+    adviser?: Adviser | null;
+    adviser_id?: number | null;
+    is_confirmed?: boolean | null;
     user: User;
     user_id: number;
-}
-
-export interface EnrolledSubject {
+    proposals: Proposal[];
+    panelists: Adviser[];
+    defense_date?: string | null;
+    defense_time?: string | null;
+  }
+  
+  export interface Proposal {
     id: number;
+    thesis: Thesis;
+    thesis_id: number;
+    file_url: string;
+    uploaded_at: string;
+  }
+  
+  export interface EnrolledSubject {
+    id: number;
+    student: Student;
     student_id: number;
     subject_name: string;
     or_number: string;
     attachment: string;
-    is_confirmed: boolean | null;
+    is_confirmed?: boolean | null;
     enrolled_at: string;
-}
+  }
+  
