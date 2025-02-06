d30ad1dc678954493f3ae076214912a94a99c54d
sggranil <dev.simongranil@gmail.com>
2025-02-06 22:53:18 +0800

    added api query for proposal

---
+++ src/app/api/thesis/proposal/add/route.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+    try {
+        const { thesis_id, file_url } = await request.json();
+
+        if (!thesis_id || !file_url || typeof file_url !== 'string') {
+            return NextResponse.json(
+                { message: "Thesis ID or file URL is missing or invalid." },
+                { status: 400 }
+            );
+        }
+
+        const thesisExists = await prisma.thesis.findUnique({
+            where: { id: Number(thesis_id) },
+        });
+
+        if (!thesisExists) {
+            return NextResponse.json(
+                { message: "Thesis ID does not exist." },
+                { status: 404 }
+            );
+        }
+
+        await prisma.proposal.create({
+            data: {
+                thesis_id: Number(thesis_id),
+                file_url: file_url,
+            },
+        });
+
+        return NextResponse.json(
+            { message: "File uploaded successfully" },
+            { status: 201 }
+        );
+    } catch (err) {
+        
+        if (err instanceof Error){
+            console.log("Error: ", err.stack)
+        }
+        return NextResponse.json(
+            { message: "An error occurred during uploading" },
+            { status: 500 }
+        );
+    }
+}
