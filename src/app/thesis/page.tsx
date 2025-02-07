8643cbaf7c128167ec0b0e7ccdb162ce61d299d9
sggranil <dev.simongranil@gmail.com>
2025-02-07 16:00:45 +0800

    initial page for thesis

--- src/app/thesis/page.tsx
+++ src/app/thesis/page.tsx
@@ -1,50 +1,38 @@
 "use client";
 
-import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { getUserRoleFromCookies } from '@/utilities/AuthUtilities';
 
 import useThesisRequest from '@/hooks/thesis';
-import { Thesis } from '@/interface/thesis.interface';
 import FormDownloadableCard from '@/components/molecules/Thesis/FormDownloadableCard';
 import ProposalCardList from '@/components/organisms/Proposal/ProposalCardList';
+import { Thesis } from '@/interface/thesis.interface';
+import ThesisInfoCard from '@/components/organisms/Thesis/ThesisInfoCard';
 
 export default function ThesisPage() {
     const [thesisData, setThesisData] = useState<Thesis | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
-
+    const [isUpdated, setIsUpdated] = useState<boolean>(false);
     const searchParams = useSearchParams();
     const thesisId = searchParams.get('id');
     const userRole = getUserRoleFromCookies();
     const { fetchThesis } = useThesisRequest();
-
-    if (!thesisId) {
-        return (
-            <div className="min-h-screen flex flex-col justify-center items-center">
-                <p>No Thesis Found!</p>
-
-                <Link className="text-blue-500 underline" href={`/dashboard/${userRole}`}>
-                    Go back to dashboard
-                </Link>
-            </div>
-        );
-    }
 
     const fetchThesisData = async () => {
         setLoading(true);
         const response = await fetchThesis(Number(thesisId));
         if (response) {
             setThesisData(response.data);
-            setLoading(false);
         }
+        setLoading(false);
     }
 
     useEffect(() => {
-        fetchThesisData();
-    }, []);
-
+        if (thesisId) fetchThesisData();
+    }, [isUpdated]);
+    
     return (
         <div className='w-full py-4'>
             {loading ? (
@@ -55,45 +43,31 @@ export default function ThesisPage() {
                 <>
                     <div className='p-4 border-b-2 border-gray-200'>
                         <h1 className='text-textPrimary text-xl md:text-2xl font-bold'>"{thesisData?.thesis_title}"</h1>
-                        <p className='font-semibold'>Adviser: {thesisData?.adviser?.id}</p>
+                        {userRole === "adviser" && (
+                            <>
+                                <span className='uppercase text-lg font-bold'>{thesisData?.student?.user?.first_name} {thesisData?.student?.user?.last_name}</span> | 
+                                <span className='uppercase text-lg font-bold'> {thesisData?.student?.user?.program}</span>
+                            </>
+                        )}
                     </div>
 
                     <div className="grid grid-cols-12 gap-2 py-2">
                         <div className="col-span-12 md:col-span-7 p-4">
-                            <ProposalCardList thesisId={thesisId} />
+                            <ProposalCardList thesisId={thesisId ?? ""} />
                         </div>
                         <div className="col-span-12 md:col-span-5">
-                            <div className='bg-gray-100 rounded-md p-2 px-4 mb-4'>
-                                <h4 className='border-b border-gray-300 py-2 font-semibold'>Information</h4>
-                                <div className='py-4'>
-                                    <p className='border-b border-gray-300 pb-2'>
-                                        <strong>Defense Date:</strong> <span>{thesisData?.defense_date ? thesisData?.defense_date : "To be Announced"}</span>
-                                    </p>
-                                    <p className='border-b border-gray-300 py-2'>
-                                        <strong>Defense Time:</strong> <span>{thesisData?.defense_time ? thesisData?.defense_time : "To be Announced"}</span>
-                                    </p>
-                                    <p className="border-b border-gray-300 py-2">
-                                        <strong>Panelist:</strong>{" "}
-                                        {!thesisData?.panelists || thesisData.panelists.length === 0 ? (
-                                            <span>No Panelist Assigned</span>
-                                        ) : (
-                                            thesisData.panelists.map((data, index) => (
-                                                <span key={index} className="block">{index}</span>
-                                            ))
-                                        )}
-                                    </p>
-
+                            <ThesisInfoCard setIsUpdated={setIsUpdated} thesisData={thesisData} />
+                            {userRole === "student" && (
+                                <div className='bg-gray-100 rounded-md p-2 px-4'>
+                                    <h1 className='border-b border-gray-300 py-2 font-semibold'>Downloadables</h1>
+                                    <div className="py-2 flex overflow-x-auto md:overflow-visible md:flex-col">
+                                        <FormDownloadableCard itemNo={1} title="Concept Paper Adviser Endorsement Form" link="#" />
+                                        <FormDownloadableCard itemNo={2} title="Thesis Dissertation Advising Contract" link="#" />
+                                        <FormDownloadableCard itemNo={3} title="Thesis Dissertation Consultation and Monitoring Form" link="#" />
+                                        <FormDownloadableCard itemNo={4} title="Thesis Dissertation Adviser Appointment and Acknowledgement Form" link="#" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='bg-gray-100 rounded-md p-2 px-4'>
-                                <h1 className='border-b border-gray-300 py-2 font-semibold'>Downloadables</h1>
-                                <div className="py-2 flex overflow-x-auto md:overflow-visible md:flex-col">
-                                    <FormDownloadableCard itemNo={1} title="Concept Paper Adviser Endorsement Form" link="#" />
-                                    <FormDownloadableCard itemNo={2} title="Thesis Dissertation Advising Contract" link="#" />
-                                    <FormDownloadableCard itemNo={3} title="Thesis Dissertation Consultation and Monitoring Form" link="#" />
-                                    <FormDownloadableCard itemNo={4} title="Thesis Dissertation Adviser Appointment and Acknowledgement Form" link="#" />
-                                </div>
-                            </div>
+                            )}
                         </div>
                     </div>
                 </>
