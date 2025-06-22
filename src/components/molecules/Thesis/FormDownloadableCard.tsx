d851d915841d5e66e1d5e72c6762f4fc1c4de844
sggranil <dev.simongranil@gmail.com>
2025-06-23 03:40:46 +0800

    modifies form downloadable card

--- src/components/molecules/Thesis/FormDownloadableCard.tsx
+++ src/components/molecules/Thesis/FormDownloadableCard.tsx
@@ -1,30 +1,31 @@
 "use client";
 
-import { DocumentProps } from "@react-pdf/renderer";
-import { ReactElement } from "react";
 import PDFDownloadWrapper from "@/components/wrapper/PDFExportWrapper";
 
 interface FormCardProps {
     itemNo: number;
     title: string;
-    document: ReactElement<DocumentProps>;
+    document: React.ReactNode;
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
 }
 
-const FormDownloadableCard: React.FC<FormCardProps> = ({ itemNo, title, document }) => {
+const FormDownloadableCard: React.FC<FormCardProps> = ({ itemNo, title, document, header, footer }) => {
     return (
         <div className="w-[300] md:w-full flex-shrink-0 m-1">
             <div className="h-[110] md:h-full flex items-center justify-between py-3 bg-white rounded-md">
                 <div>
                     <h4 className="text-sm font-semibold text-textPrimary mb-2">TWD Form No. {itemNo}: {title}</h4>
                     <PDFDownloadWrapper
-                        document={document}
-                        fileName={`${title}.pdf`}
+                        header={header}
+                        content={document}
+                        footer={footer}
+                        fileName={`${title}`}
                         buttonLabel={`Download Form #${itemNo}`}
                     />
                 </div>
             </div>
         </div>
-
     );
 }
 
