2968e6d392ecd7aee16408cfc9e72bf6db807dae
sggranil <dev.simongranil@gmail.com>
2025-12-03 10:11:09 +0800

    refactor navbar components

--- src/components/organisms/NavbarLinks.tsx
+++ src/components/organisms/NavbarLinks.tsx
@@ -1,50 +1,48 @@
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-interface NavbarLinksProps {
+interface NavbarLinksProps {}
 
-}
+const NavbarLinks: React.FC<NavbarLinksProps> = ({}) => {
+  const pathname = usePathname();
 
-const NavbarLinks: React.FC<NavbarLinksProps> = ({ }) => {
-    const pathname = usePathname();
+  const BASE_CLASS = "text-content-secondary";
+  const ACTIVE_CLASS =
+    "border-b-2 border-brand-primary text-brand-primary font-bold";
 
-    const BASE_CLASS = "flex flex-col items-center justify-center text-content-secondary hover:text-brand-primary-hover transition-colors border-b-2 border-transparent px-4 md:py-2 py-3";
-    const ACTIVE_CLASS = "text-red-900 font-bold border-brand-primary hover:text-brand-primary hover:border-brand-primary";
+  const getLinkClassName = (href: string) => {
+    const isActive = pathname.startsWith(href) && href !== "/";
+    const classNames = isActive ? `${ACTIVE_CLASS}` : BASE_CLASS;
 
-    const getLinkClassName = (href: string) => {
-        const isActive = pathname.startsWith(href) && href !== '/';
+    return classNames;
+  };
 
-        const classNames = isActive
-            ? `${BASE_CLASS} ${ACTIVE_CLASS}`
-            : BASE_CLASS;
+  const navItems = [
+    { href: "/thesis", label: "Thesis", icon: "library_books" },
+    { href: "/notification", label: "Notification", icon: "notifications" },
+  ];
 
-        return classNames;
-    };
-
-    const navItems = [
-        { href: "/thesis", label: "Thesis", icon: "library_books" },
-        { href: "/notification", label: "Notification", icon: "notifications" },
-    ];
-
-    return (
-        <div className="flex items-center">
-            {navItems.map((item) => (
-                <Link
-                    key={item.href}
-                    href={item.href}
-                    className={getLinkClassName(item.href)}
-                >
-                        <span className="material-symbols-outlined">{item.icon}</span>
-                        
-                    <span className="hidden md:block text-xs font-normal">{item.label}</span>
-                </Link>
-            ))}
-            <button className="flex flex-row items-center justify-center text-content-primary px-4 border-l-2 border-gray-200 md:pl-4">
-                <span className="material-symbols-outlined">account_circle</span>
-                <span className="material-symbols-rounded text-content-secondary">arrow_drop_down</span>
-            </button>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center">
+      {navItems.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          className={`flex flex-col items-center justify-center hover:text-brand-primary-hover transition-colors px-4 md:py-2 py-3 ${getLinkClassName(
+            item.href
+          )}`}>
+          <span className="material-symbols-outlined">{item.icon}</span>
+          <span className="hidden md:block text-xs">{item.label}</span>
+        </Link>
+      ))}
+      <button className="flex flex-row items-center justify-center text-content-primary border-l-2 border-gray-200 pl-4">
+        <span className="material-symbols-outlined">account_circle</span>
+        <span className="material-symbols-rounded text-content-secondary">
+          arrow_drop_down
+        </span>
+      </button>
+    </div>
+  );
+};
 
 export default NavbarLinks;
--- src/components/template/Navbar.tsx
+++ src/components/template/Navbar.tsx
@@ -1,33 +1,33 @@
 import NavbarLinks from "@/components/organisms/NavbarLinks";
 
 const Navbar = ({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) => {
-    return (
-        <div className="w-full">
-            <div className="fixed top-0 left-0 right-0 flex md:justify-evenly justify-between items-center w-full shadow-md z-50 bg-white px-4">
-                <div className="flex items-center">
-                    <img
-                        src="/pup.png"
-                        alt="Logo"
-                        className="w-10 h-10 mr-3"
-                    />
-                    <div className="flex flex-col space-y-0.05">
-                        <span className="hidden md:block text-xl font-bold text-content-primary">PUP</span>
-                        <span className="hidden md:block text-sm font-normal text-content-secondary whitespace-nowrap">Graduate Thesis Monitoring</span>
-                    </div>
-                </div>
-
-                <NavbarLinks />
-            </div>
-
-            <main className="flex flex-col items-center w-full bg-app-surface min-h-screen overflow-hidden pt-16">
-                {children}
-            </main>
+  return (
+    <div className="w-full">
+      <div className="fixed top-0 left-0 right-0 flex md:justify-evenly justify-between items-center w-full shadow-md z-50 bg-white px-4">
+        <div className="flex items-center">
+          <img src="/pup.png" alt="Logo" className="w-10 h-10 mr-3" />
+          <div className="flex flex-col space-y-0.05">
+            <span className="hidden md:block text-xl font-bold text-content-primary">
+              PUP
+            </span>
+            <span className="hidden md:block text-sm font-normal text-content-secondary whitespace-nowrap">
+              Graduate School
+            </span>
+          </div>
         </div>
-    )
-}
+
+        <NavbarLinks />
+      </div>
+
+      <main className="flex flex-col items-center w-full bg-app-surface min-h-screen overflow-hidden pt-16">
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export default Navbar;
