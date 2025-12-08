72bd3bf83d3ec05c87f2b4ebc00f13ac818604bf
sggranil <dev.simongranil@gmail.com>
2025-12-08 09:34:26 +0800

    create query provider for react useQuery

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // Optional but recommended
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,
+        },
+    },
+});
+
+export function QueryProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+        </QueryClientProvider>
+    );
+}
