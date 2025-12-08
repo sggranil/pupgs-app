54b2f3357a0ac97a87777390551a0a489e86d71d
sggranil <dev.simongranil@gmail.com>
2025-12-08 15:27:00 +0800

    :wip: query clean up on attachments

--- src/hooks/attachment.ts
+++ src/hooks/attachment.ts
@@ -1,37 +1,64 @@
-import { requestHandler } from "@/services/RequestServices";
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { ATTACHMENT_API } from "@/constants/urls";
 
-const useAttachmentRequest = () => {
-    const getAttachment = async (id: number) => {
-        const response = await requestHandler({
-            method: 'GET',
-            url: ATTACHMENT_API.GET_ATTACHMENT_URL(id)
-        })
-        return response.json()
-    }
+import { handleGetRequest, handlePostRequest } from "@/hooks/request"; 
+import { ResponsePayloadResult } from '@/interface/request.interface';
 
-    const addAttachment = async (body: object) => {
-        const response = await requestHandler({
-            method: 'POST',
-            body: JSON.stringify(body),
-            url: ATTACHMENT_API.ADD_ATTACHMENT_URL
-        })
-        return response.json()
-    }
+const ATTACHMENT_KEYS = {
+    all: ['attachment'] as const,
+    lists: () => [...ATTACHMENT_KEYS.all, 'list'] as const,
+    detail: (id: number) => [...ATTACHMENT_KEYS.all, 'detail', id] as const,
+    user: (id: number) => [...ATTACHMENT_KEYS.all, 'user', id] as const,
+};
 
-    const deleteAttachment = async (id: number) => {
-        const response = await requestHandler({
-            method: 'POST',
-            url: ATTACHMENT_API.DELETE_ATTACHMENT_URL(id)
-        })
-        return response.json()
-    }
+export const useGetAttachment = (id: number | undefined | null) => {
+    const isEnabled = !!id;
 
-    return {
-        getAttachment,
-        addAttachment,
-        deleteAttachment
-    }
+    return useQuery<ResponsePayloadResult, Error>({
+        queryKey: ATTACHMENT_KEYS.detail(id as number),
+        queryFn: () => handleGetRequest<ResponsePayloadResult>(ATTACHMENT_API.GET_ATTACHMENT_URL(id as number)),
+        enabled: isEnabled,
+    });
+};
+
+const AttachmentService = {
+    useGetAttachment
 }
 
-export default useAttachmentRequest;
+export default AttachmentService;
+
+
+// const useAttachmentRequest = () => {
+//     const getAttachment = async (id: number) => {
+//         const response = await requestHandler({
+//             method: 'GET',
+//             url: ATTACHMENT_API.GET_ATTACHMENT_URL(id)
+//         })
+//         return response.json()
+//     }
+
+//     const addAttachment = async (body: object) => {
+//         const response = await requestHandler({
+//             method: 'POST',
+//             body: JSON.stringify(body),
+//             url: ATTACHMENT_API.ADD_ATTACHMENT_URL
+//         })
+//         return response.json()
+//     }
+
+//     const deleteAttachment = async (id: number) => {
+//         const response = await requestHandler({
+//             method: 'POST',
+//             url: ATTACHMENT_API.DELETE_ATTACHMENT_URL(id)
+//         })
+//         return response.json()
+//     }
+
+//     return {
+//         getAttachment,
+//         addAttachment,
+//         deleteAttachment
+//     }
+// }
+
+// export default useAttachmentRequest;
