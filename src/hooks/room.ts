548d6fc12daf471c5d91af7126b35838433da4e7
sggranil <dev.simongranil@gmail.com>
2025-12-09 23:02:09 +0800

    apply useQuery hook to rooms

--- src/hooks/room.ts
+++ src/hooks/room.ts
@@ -1,46 +1,51 @@
-import { requestHandler } from "@/services/RequestServices";
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { ROOM_API } from "@/constants/urls";
 
-const useRoomRequest = () => {
-    const getAllRooms = async () => {
-        const response = await requestHandler({
-            method: 'GET',
-            url: ROOM_API.GET_ALL_ROOM_URL
-        })
-        return response.json()
-    }
+import { handleGetRequest, handlePostRequest } from "@/hooks/request";
+import { ResponsePayloadResult } from '@/interface/request.interface';
+import { Room } from '@/interface/thesis.interface';
 
-    const getAvailableRoom = async (id: number, date: string) => {
-        const response = await requestHandler({
-            method: 'GET',
-            url: ROOM_API.AVAILABLE_ROOM_URL(id, date)
-        })
-        return response.json()
-    }
+const ROOM_KEYS = {
+    all: ['room'] as const,
+    lists: () => [...ROOM_KEYS.all, 'list'] as const,
+    detail: (date: Date) => [...ROOM_KEYS.all, 'detail', date] as const,
+    user: (id: number) => [...ROOM_KEYS.all, 'user', id] as const,
+};
 
-    const addRoom = async (body: object) => {
-        const response = await requestHandler({
-            method: 'POST',
-            body: JSON.stringify(body),
-            url: ROOM_API.ADD_ROOM_URL
-        })
-        return response.json()
-    }
+export const useAllRooms = () => {
+    return useQuery<Room[], Error>({
+        queryKey: ROOM_KEYS.lists(),
+        queryFn: () => handleGetRequest<Room[]>(ROOM_API.GET_ALL_ROOM_URL),
+    });
+};
 
-    const deleteRoom = async (id: number) => {
-        const response = await requestHandler({
-            method: 'POST',
-            url: ROOM_API.DELETE_ROOM_URL(id)
-        })
-        return response.json()
-    }
+export const useAvailableRooms = (date: Date | undefined | null) => {
+    const isEnabled = !!date;
 
-    return {
-        getAllRooms,
-        getAvailableRoom,
-        addRoom,
-        deleteRoom
-    }
+    return useQuery<ResponsePayloadResult, Error>({
+        queryKey: ROOM_KEYS.detail(date as Date),
+        queryFn: () => handleGetRequest<ResponsePayloadResult>(ROOM_API.AVAILABLE_ROOM_URL(date as Date)),
+        enabled: isEnabled,
+    });
+};
+
+export const useAddRoom = () => {
+    return useMutation<Room, Error, object>({
+        mutationFn: (body: any) => handlePostRequest<Room>(ROOM_API.ADD_ROOM_URL, body),
+    });
+};
+
+export const useDeleteRoom = () => {
+    return useMutation<{ message: string }, Error, number>({
+        mutationFn: (id: number) => handlePostRequest<{ message: string }>(ROOM_API.DELETE_ROOM_URL(id)),
+    });
+};
+
+const RoomService = {
+    useAllRooms,
+    useAvailableRooms,
+    useAddRoom,
+    useDeleteRoom
 }
 
-export default useRoomRequest;
+export default RoomService;
