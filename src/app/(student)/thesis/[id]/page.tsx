c832115bddec82b1b32e2c32002f65d7b221f5bb
sggranil <dev.simongranil@gmail.com>
2025-12-10 01:32:51 +0800

    added thesis information

--- src/app/(student)/thesis/[id]/page.tsx
+++ src/app/(student)/thesis/[id]/page.tsx
@@ -241,59 +241,68 @@ import ThesisInfoLoadingState from "@/components/template/SkeletonContainer/Thes
 import { ThesisTitle } from "@/components/organisms/Thesis/ThesisTitle";
 import AttachmentCardList from "@/components/organisms/Attachment/AttachmentCardList";
 
-export default function ThesisInfoPage({ params }: { params: Promise<{ id: number }> }) {
+import ThesisInformation from "@/components/organisms/Thesis/ThesisInformation";
+
+export default function ThesisInfoPage({
+  params,
+}: {
+  params: Promise<{ id: number }>;
+}) {
   const { id } = use(params);
 
   const {
     data: thesisData,
     isLoading: isThesisLoading,
     error,
-    refetch
+    refetch,
   } = useGetThesis(id);
 
   const handleThesisUpdated = () => {
     refetch();
   };
 
-  const thesisInfo = (thesisData?.data as Thesis);
+  const thesisInfo = thesisData?.data as Thesis;
 
   if (isThesisLoading) {
-    return <ThesisInfoLoadingState />
+    return <ThesisInfoLoadingState />;
   }
 
   return (
     <>
       <div className="flex flex-col items-start justify-center md:flex-row gap-4 w-full lg:px-32 py-4 px-8">
-        <div className="flex flex-col w-full md:w-1/2 gap-4">
-          {/* Header Panel */}
+        <div className="flex flex-col w-full md:w-1/3 gap-4">
           <div className="bg-white ring-1 ring-black ring-opacity-10 transition-opacity px-4 pt-2 pb-2 rounded-md">
             <h1 className="text-content-primary text-lg font-bold pb-1 pt-2">
-              <ThesisTitle thesisId={thesisInfo.id} thesisTitle={thesisInfo.thesis_title} setIsUpdated={handleThesisUpdated} />
+              <ThesisTitle
+                thesisId={thesisInfo.id}
+                thesisTitle={thesisInfo.thesis_title}
+                setIsUpdated={handleThesisUpdated}
+              />
             </h1>
             <div className="mb-2">
-              <span className="text-content-primary font-normal pr-2">
-                {thesisInfo.student?.user.last_name}, {" "}
+              <p className="text-content-primary font-semibold text-sm pr-2">
+                {thesisInfo.student?.user.last_name},{" "}
                 {thesisInfo.student?.user.first_name}
-              </span>
-              <span className="border-l-2 border-grey-800 pl-2">{thesisInfo.student?.user.program}</span>
+              </p>
+              <p className="font-normal text-sm">
+                {thesisInfo.student?.user.program}
+              </p>
             </div>
           </div>
-
-          {/* Attachments Panel */}
-          <div className="bg-white ring-1 ring-black ring-opacity-10 transition-opacity px-4 pt-2 pb-2 rounded-md">
-            <h1 className="text-content-primary text-md font-bold pb-1 pt-2">
-              Attachments
-            </h1>
-            <AttachmentCardList thesisId={thesisInfo.id} status={""} />
+          <div className="bg-white ring-1 ring-black ring-opacity-10 transition-opacity px-4 pb-2 rounded-md">
+            <ThesisInformation
+              thesisData={thesisInfo}
+              setIsUpdated={handleThesisUpdated}
+            />
           </div>
         </div>
 
-        <div className="bg-white w-full md:w-1/3 ring-1 ring-black ring-opacity-10 transition-opacity px-4 pt-2 pb-2 rounded-md">
-          <div className="flex flex-col justify-between">
-            <h3 className="text-content-primary text-md font-bold pb-1 pt-2">
-              Information
-            </h3>
-          </div>
+        <div className="bg-white w-full md:w-1/2 ring-1 ring-black ring-opacity-10 transition-opacity px-4 pb-2 rounded-md">
+          <AttachmentCardList
+            thesisId={thesisInfo.id}
+            attachments={thesisInfo.attachments}
+            setIsUpdated={handleThesisUpdated}
+          />
         </div>
       </div>
     </>
