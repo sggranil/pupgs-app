346ceaa78aba59729de10ddeb63c881e5396b6aa
sggranil <dev.simongranil@gmail.com>
2025-12-11 14:50:52 +0800

    rename and refactor thesis receipts card

---
+++ src/components/molecules/ThesisReceiptsCard.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+
+import { ThesisReceipt } from "@/interface/thesis.interface";
+import { formatStatus } from "@/utilities/StringFormatter";
+
+import Modal from "@/components/organisms/Modal";
+import ManageThesisReceiptModal from "@/components/organisms/Modals/ManageThesisReceiptModal";
+
+interface ThesisReceiptsCardProps {
+  userId: number;
+  thesisId: number;
+  receipts: ThesisReceipt;
+  setIsUpdated: (isUpdated: boolean) => void;
+}
+
+const ThesisReceiptsCard: React.FC<ThesisReceiptsCardProps> = ({
+  userId,
+  thesisId,
+  receipts,
+  setIsUpdated,
+}) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  if (isModalOpen) {
+    return (
+      <Modal
+        title="Upload Receipt"
+        isModalOpen={isModalOpen}
+        setModalOpen={setModalOpen}
+        modalType="info">
+        <ManageThesisReceiptModal
+          userId={userId}
+          thesisId={thesisId}
+          receiptData={receipts}
+          setIsModalOpen={setModalOpen}
+          setIsUpdated={setIsUpdated}
+        />
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+      <div className="w-full ring-1 ring-black ring-opacity-10 transition-opacity rounded-sm cursor-pointer">
+        <div
+          onClick={() => setModalOpen(true)}
+          className="flex items-center justify-between py-2 px-3">
+          <div>
+            <h2 className="text-sm font-bold text-content-primary">
+              {formatStatus(receipts.receipt_name)}
+            </h2>
+            <p className="text-xs font-semibold text-content-primary">
+              OR #{receipts.or_number}
+            </p>
+            <p className="text-xs text-gray-500 pt-1">
+              Status: {formatStatus(receipts.status)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ThesisReceiptsCard;
---
+++ src/components/organisms/ThesisReceipt/ThesisReceiptCardList.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import {
+  Attachment,
+  Thesis,
+  ThesisReceipt,
+} from "@/interface/thesis.interface";
+
+import Modal from "@/components/organisms/Modal";
+
+import ActionButton from "@/components/molecules/ActionButton";
+import ThesisReceiptsCard from "@/components/molecules/ThesisReceiptsCard";
+import ManageThesisReceiptModal from "@/components/organisms/Modals/ManageThesisReceiptModal";
+
+interface ThesisReceiptCardListProps {
+  userId: number;
+  thesisId: number;
+  receipts?: ThesisReceipt[];
+  setIsUpdated: (isUpdated: boolean) => void;
+}
+
+const ThesisReceiptCardList: React.FC<ThesisReceiptCardListProps> = ({
+  userId,
+  receipts,
+  thesisId,
+  setIsUpdated,
+}) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="w-full">
+        <div className="flex items-center justify-between pt-4 pb-3 ">
+          <h3 className="text-content-primary text-md font-bold">
+            Thesis Receipts
+          </h3>
+          {/* {userData?.role === "admin" && ( */}
+          <ActionButton
+            icon="upload"
+            label="Upload"
+            onClick={() => setModalOpen(true)}
+          />
+        </div>
+        {receipts!.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 pb-2">
+            {receipts?.map((receipt, index) => (
+              <ThesisReceiptsCard
+                userId={userId}
+                thesisId={thesisId}
+                key={index}
+                receipts={receipt}
+                setIsUpdated={setIsUpdated}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500 text-sm text-center py-12">
+            No thesis receipts yet.
+          </p>
+        )}
+      </div>
+      <Modal
+        title="Upload Receipt"
+        isModalOpen={isModalOpen}
+        setModalOpen={setModalOpen}
+        modalType="info">
+        <ManageThesisReceiptModal
+          userId={userId}
+          thesisId={thesisId}
+          setIsModalOpen={setModalOpen}
+          setIsUpdated={setIsUpdated}
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default ThesisReceiptCardList;
