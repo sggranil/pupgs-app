b5cedff0c767249860738d0f68aa33f128df00d8
sggranil <dev.simongranil@gmail.com>
2025-12-11 08:04:11 +0800

    update adviser hook

--- src/hooks/adviser.ts
+++ src/hooks/adviser.ts
@@ -1,18 +1,25 @@
-import { requestHandler } from "@/services/RequestServices";
+import { useQuery } from '@tanstack/react-query';
 import { ADVISER_API } from "@/constants/urls";
 
-const useAdviserRequest = () => {
-    const getAllAdviser = async () => {
-        const response = await requestHandler({
-            method: 'GET',
-            url: ADVISER_API.GET_ADVISERS_URL
-        })
-        return response.json()
-    }
+import { handleGetRequest } from "@/hooks/request";
+import { ResponsePayloadResult } from '@/interface/request.interface';
 
-    return {
-        getAllAdviser,
-    }
+const ADVISER_KEYS = {
+    all: ['room'] as const,
+    lists: () => [...ADVISER_KEYS.all, 'list'] as const,
+    detail: (date: Date) => [...ADVISER_KEYS.all, 'detail', date] as const,
+    user: (id: number) => [...ADVISER_KEYS.all, 'user', id] as const,
+};
+
+export const useAllAdvisers = () => {
+    return useQuery<ResponsePayloadResult, Error>({
+        queryKey: ADVISER_KEYS.lists(),
+        queryFn: () => handleGetRequest<ResponsePayloadResult>(ADVISER_API.GET_ADVISERS_URL),
+    });
+};
+
+const AdviserService = {
+    useAllAdvisers,
 }
 
-export default useAdviserRequest;
+export default AdviserService;
